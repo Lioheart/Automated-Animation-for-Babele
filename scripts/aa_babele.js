dfa8d4d0ffa9550406296c0d62555aa04a7329f7
Jakub <caco7.lioheart@gmail.com>
2026-04-24 18:24:46 +0200

    For Foundry v14

--- scripts/aa_babele.js
+++ scripts/aa_babele.js
@@ -1,17 +1,28 @@
+const { getProperty } = foundry.utils;
+
 const capitalize = (str, lower = false) =>
   (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, match => match.toUpperCase());
-;
 
 Hooks.on("aa.getRequiredData", (data) => {
     console.log("aa.getRequiredData hook fired", data);
+
+    if (!data?.item) return;
+
     let originalName = getProperty(data.item, "flags.babele.originalName");
 
     if (!originalName) {
-        originalName = getProperty(data.item, "system.identifier");
-        originalName = capitalize(originalName.replaceAll("-", " "));
+        const identifier = getProperty(data.item, "system.identifier");
+
+        if (typeof identifier === "string" && identifier.length) {
+            originalName = capitalize(identifier.replaceAll("-", " "));
+        }
     }
 
     if (!originalName) return;
+
+    if (!Array.isArray(data.overrideNames)) {
+        data.overrideNames = [];
+    }
 
     data.overrideNames.push(originalName);
 });
